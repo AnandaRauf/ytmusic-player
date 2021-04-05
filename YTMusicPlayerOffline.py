68fbffb0576bfab03e21546298f43123eddf6873
Ananda Rauf <uzumaki.nanda@rocketmail.com>
2021-04-05 13:40:44 +0700

    Add files via upload
    Script Project

---
+++ YTMusicPlayerOffline.py
@@ -0,0 +1,28 @@
+print("--------------------------------------------------------------\n")
+programname = "YT Music Player"
+version = "1.0"
+devdate = "5 April 2021"
+devby= "Hilruf"
+print(programname)
+print(version)
+print(devdate)
+print(devby)
+print("--------------------------------------------------------------\n")
+
+class Carimusik:
+    def carimusik():
+        
+        #Api key Youtube:
+        print("Syntax Search Youtube")
+        
+
+class Listmusik():
+    def listmusik():
+        print("Nadin Amisah - Bertaut")
+class Pemutarmusik:
+    def pemutarmusik():
+        
+       Listmusik.listmusik() 
+
+Carimusik.carimusik()
+Pemutarmusik.pemutarmusik()
